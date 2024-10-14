77e4ddc20541a8052f0ffdf6b8f3bad7445f9c8b
Dale Chang <dalecb13@gmail.com>
2024-10-14 22:04:05 +0200

    added images

--- components/goals-section.vue
+++ components/goals-section.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="my-16 mx-8">
-    <h2 class="mb-8 text-2xl">Goals</h2>
+  <div>
+    <article class="my-16 mx-8">
+      <h2 class="mb-8 text-2xl">Goals</h2>
 
-    <p class="mb-8">I approach goal setting with clients as an ongoing thing. I prefer to have a goal for each session and like to discuss that goal at the beginning of every meeting to help the session stay focused and productive for the client. I believe that there is no goal that is too small and that understanding one's goals can help the client see a bigger picture. </p>
+      <p class="mb-8">I approach goal setting with clients as an ongoing thing. I prefer to have a goal for each session and like to discuss that goal at the beginning of every meeting to help the session stay focused and productive for the client. I believe that there is no goal that is too small and that understanding one's goals can help the client see a bigger picture. </p>
+    </article>
+
+    <NuxtImg src="/micheile-henderson-MG4G0congO0-unsplash.jpg" />
   </div>
 </template>
--- components/style-section.vue
+++ components/style-section.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="my-16 mx-8">
-    <h2 class="mb-8 text-2xl">Style</h2>
+  <div>
+    <article class="my-16 mx-8">
+      <h2 class="mb-8 text-2xl">Style</h2>
 
-    <p class="mb-8">The therapeutic approaches that I tend to use with clients is focused on CBT, DBT, ACT, and mindfulness/somatic based strategies. I enjoy challenging a client's mindset in order to help them gain more insight into how their thoughts and behavior patterns affect them both positively and negatively. I believe that all these approaches are a collaborative process between a client and therapist so that the client can eventually become their own therapist in to target their own needs and move towards their goals. </p>
+      <p class="mb-8">The therapeutic approaches that I tend to use with clients is focused on CBT, DBT, ACT, and mindfulness/somatic based strategies. I enjoy challenging a client's mindset in order to help them gain more insight into how their thoughts and behavior patterns affect them both positively and negatively. I believe that all these approaches are a collaborative process between a client and therapist so that the client can eventually become their own therapist in to target their own needs and move towards their goals. </p>
 
-    <p>My communication style as a therapist is warm, open, and collaborative. I strive to create a safe and non-judgmental space where clients feel heard and understood. I aim to be clear and direct when explaining concepts, while also being sensitive to each client's emotional needs. I listen deeply and ask thoughtful questions to help clients explore their thoughts and feelings. My approach is adaptable, so whether clients need gentle encouragement or more structured guidance, I tailor my communication to meet them where they are at in their journey. </p>
+      <p>My communication style as a therapist is warm, open, and collaborative. I strive to create a safe and non-judgmental space where clients feel heard and understood. I aim to be clear and direct when explaining concepts, while also being sensitive to each client's emotional needs. I listen deeply and ask thoughtful questions to help clients explore their thoughts and feelings. My approach is adaptable, so whether clients need gentle encouragement or more structured guidance, I tailor my communication to meet them where they are at in their journey. </p>
+    </article>
+
+    <NuxtImg src="/arno-smit-iI72r3gSwWY-unsplash.jpg" />
   </div>
 </template>
